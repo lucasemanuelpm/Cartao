8d746bb88f086acee737daf185b271622ffdd50c
Lucas Emanuel Perim Martins <142339368+lucasemanuelpm@users.noreply.github.com>
2024-11-28 20:20:58 -0300

    sss

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-function criaCartao(categoria, pergunta, resposta){
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral.'
+)
+
+function criaCartao(nome, email , telefone){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao' ;
     cartao.innerHTML = `
     <div class="conteudo-cartao">
-    <h3>${categoria}</h3>
+    <h3>${nome}</h3>
     <div class="pergunta-cartao">
-        <p>${pergunta}</p>
+        <p>${email}</p>
     </div>
 <div class="resposta-cartao">
-      <p>${resposta}</p>
+      <p>${telefone}</p>
 </div>
 </div>
     `
